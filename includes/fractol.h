a70ae0657d068d946934be5d721f2eee40a09683
El houcine  El yakoubi <elel-yak@e1r6p11.1337.ma>
2023-01-23 10:30:09 +0100

    add files split the work part 2

--- includes/fractol.h
+++ includes/fractol.h
@@ -20,6 +20,19 @@
 # include "structs.h"
 
 int	mandelbrot(long double x, long double y);
+int	julia(long double x, long double y);
+int	burning_ship(long double x, long double y);
+void ft_zoom(int botton, int x, int y, t_fractol *fractol);
 int	get_color(int i);
+void	ft_putstr(char *str);
+int	ft_strcmp(char *s1, char *s2);
+int	render(t_fractol *fractol);
+void	init_render(void);
+int mouse_press(int button, int x, int y, t_fractol *fractol);
+int 	close_window(t_fractol *fractol);
+int 	botton_press(int botton, t_fractol *fractol);
+void 	init_cords(t_fractol *fractol);
+void	my_mlx_pixel_put(t_fractol *fractol, int x, int y, int color);
+
 
 #endif
